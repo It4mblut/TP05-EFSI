0cfb05688213c0cf99aad3b1eadeccb66825d75d
EITAN URIEL KOZE <49433681@ort>
2026-04-22 09:04:45 -0300

    cambie el api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-const api = axios.creat({
-    baseURL: 'https://imdb.iamidiotareyoutoo.com/search?q=&tt=&lsn=1&v=1'
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3'
 })
 
 export default api
-
-//https://imdb.iamidiotareyoutoo.com/docs/index.html#GET/search
